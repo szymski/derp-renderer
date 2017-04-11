1c6bb7fea1efc1a9b8bdae859138debfdb50e196
Szymon Jankowski <szymskipl@gmail.com>
2017-04-11 11:49:17 +0200

    Rendering pixels

--- source/app.d
+++ source/app.d
@@ -1,6 +1,109 @@
 import std.stdio;
 
+import std.stdio, std.datetime, std.file, std.string, core.thread, std.experimental.logger, derelict.sdl2.sdl, derelict.opengl3.gl;
+import renderer.renderer;
+
+SDL_Window* window;
+SDL_Renderer* sdlRenderer;
+
+bool running = true;
+
+enum width = 128;
+enum height = 128;
+
+Renderer myRenderer;
+Color[] pixels;
+
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	myRenderer = new Renderer(width, height);
+	pixels = myRenderer.render();
+
+	start();
+}
+
+void start() {
+	log("Starting derp-renderer");
+	loadLibraries();
+	openWindow();
+	enterLoop();
+}
+
+private void loadLibraries() {
+	log("Loading libraries");
+	DerelictSDL2.load("lib/SDL2");
+	DerelictGL.load();
+}
+
+private void openWindow() {
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	
+	log("Creating window");
+	SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_OPENGL, &window, &sdlRenderer);
+	SDL_SetWindowTitle(window, "Gay Boy Emulator".toStringz);
+	log("Creating OpenGL context");
+	SDL_GL_CreateContext(window);
+}
+
+private void enterLoop() {
+	log("Entering main loop");
+	
+	while(running) {
+		updateEvents();
+		render();
+		limitFps();
+	}
+}
+
+private void updateEvents() {
+	SDL_Event event;
+	
+	while(SDL_PollEvent(&event)) {
+		switch(event.type) {
+			case SDL_QUIT:
+				running = false;
+				break;
+				
+			default:
+				break;
+		}
+	}
+}
+
+private void render() {
+	glClearColor(0.1f, 0.1f, 0.1f, 1f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0, 0, 800, 600);
+	glOrtho(0, 800, 0, 600, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	glRasterPos3f(0, 600, 0);
+	glPixelZoom(1f, -1f);
+	glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, pixels.ptr);
+
+	SDL_GL_SwapWindow(window);
+}
+
+private void limitFps() {
+	static StopWatch sw = StopWatch();
+	enum maxFPS = 60;
+	
+	if(maxFPS != -1) {
+		long desiredNs = 1_000_000_000 / maxFPS; // How much time the frame should take
+		
+		if(desiredNs - sw.peek.nsecs >= 0)
+			Thread.sleep(nsecs(desiredNs - sw.peek.nsecs));
+		
+		sw.reset();
+		sw.start();
+	}
 }
---
+++ source/renderer/renderer.d
@@ -0,0 +1,37 @@
+﻿module renderer.renderer;
+
+struct Color {
+	ubyte r, g, b;
+
+	this(ubyte r = 0, ubyte g = 0, ubyte b = 0) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+}
+
+class Renderer
+{
+	int width, height;
+
+	this(int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+	}
+
+	Color[] render() {
+		Color[] pixels = new Color[height * width];
+
+		import std.random;
+
+		foreach(y; 0 .. height) {
+			foreach(x; 0 .. width) {
+				pixels[y * width + x].r = cast(ubyte)uniform(0, 255);
+			}
+		}
+
+		return pixels;
+	}
+}
+
